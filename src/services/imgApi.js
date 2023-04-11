6612d32798bcb648b03edb6cc584bf665bb2c3d3
RostyslavSuprun <kredlik62@gmail.com>
2023-04-11 20:08:26 +0300

    fixed key error

--- src/services/imgApi.js
+++ src/services/imgApi.js
@@ -1,6 +1,6 @@
 export default async function getImages(inputValue, page = 1) {
   const url = 'https://pixabay.com/api/';
-  const API_KEY = '31248055-e075bd58b7f60ee40b8d7aef1';
+  const API_KEY = '31253077-0a51af2ed3c94e9419b38a769&';
 
   return await fetch(
     `${url}?q=${inputValue}&page=${page}&key=${API_KEY}&image_type=photo&orientation=horizontal&per_page=12`
